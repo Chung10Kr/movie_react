29aec1621702b43d0cf28d757bb8a51afe6dec32
unknown <2017610089@sdu.ac.kr>
2020-02-02 19:10:45 +0900

    Updates

---
+++ precache-manifest.30fe5537662d1c091f94529f14f81fea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8af6c76bca3c768fb3753298d6cb550",
+    "url": "/react_sampe/index.html"
+  },
+  {
+    "revision": "f2ecf72c41706ba61d74",
+    "url": "/react_sampe/static/css/main.f5618fc3.chunk.css"
+  },
+  {
+    "revision": "975c1a2c1dfdbd009111",
+    "url": "/react_sampe/static/js/2.abca4018.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_sampe/static/js/2.abca4018.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f2ecf72c41706ba61d74",
+    "url": "/react_sampe/static/js/main.b08012c6.chunk.js"
+  },
+  {
+    "revision": "8dfd8a3df2ce7fecb2a1",
+    "url": "/react_sampe/static/js/runtime-main.49d99785.js"
+  }
+]);
