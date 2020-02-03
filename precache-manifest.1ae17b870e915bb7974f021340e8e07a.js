58196110da08696ff57bc962e4465b8d1ebed499
unknown <2017610089@sdu.ac.kr>
2020-02-03 22:17:51 +0900

    Updates

---
+++ precache-manifest.1ae17b870e915bb7974f021340e8e07a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0edad2b851cb0c87f1678f7d8d587c3a",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "ce5356700d3faf0a70a4",
+    "url": "/movie_app_2019/static/css/main.45122220.chunk.css"
+  },
+  {
+    "revision": "2baf1e1f5ab767ca97cc",
+    "url": "/movie_app_2019/static/js/2.3033b0ce.chunk.js"
+  },
+  {
+    "revision": "ce5356700d3faf0a70a4",
+    "url": "/movie_app_2019/static/js/main.4a8dedc6.chunk.js"
+  },
+  {
+    "revision": "5a2b9365f7ca01d151d6",
+    "url": "/movie_app_2019/static/js/runtime~main.64ac130e.js"
+  }
+]);
